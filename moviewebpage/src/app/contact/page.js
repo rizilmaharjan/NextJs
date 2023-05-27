3082c26935555c6d2b6592f074ebad0fb841766b
rizilmaharjan <rizilmaharjan@gmail.com>
2023-05-27 16:17:04 +0545

    fixed responsiveness

--- moviewebpage/src/app/contact/page.js
+++ moviewebpage/src/app/contact/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import ContactCard from "../components/ContactCard";
 import { AiOutlineMail, AiOutlineWechat } from "react-icons/ai";
 import { MdForum } from "react-icons/md";
@@ -7,76 +7,86 @@ import { useState } from "react";
 import Maps from "../components/Maps";
 
 const page = () => {
-  const [input, setInput] = useState({})
-  const [status, setStatus] = useState(null)
+  const [input, setInput] = useState({});
+  const [status, setStatus] = useState(null);
 
-  const handleChange = (e)=>{
+  const handleChange = (e) => {
     e.preventDefault();
-    setInput(
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
 
-      { ...input,
-        [e.target.name] : e.target.value
-      }
-    )
-
-  }
-
-  const handleSubmit = async()=>{
+  const handleSubmit = async () => {
     try {
-      const response = await fetch('/api/contact',{
-        method: 'POST',
-        headers:{"Content_Type": "application/json"},
-        body:JSON.stringify({
-          username:value.username,
-          email:value.email,
-          phone:value.phone,
-          message:value.message
-        })
-      })
+      const response = await fetch("/api/contact", {
+        method: "POST",
+        headers: { Content_Type: "application/json" },
+        body: JSON.stringify({
+          username: value.username,
+          email: value.email,
+          phone: value.phone,
+          message: value.message,
+        }),
+      });
       // set the status based on the response from the API route
-      if(response.status === 200){
+      if (response.status === 200) {
         setInput({
-          username:"",
-          email:"",
-          phone:"",
-          message:""
-        })
-        setStatus("success")
-
-      }else{
-        setStatus("error")
+          username: "",
+          email: "",
+          phone: "",
+          message: "",
+        });
+        setStatus("success");
+      } else {
+        setStatus("error");
       }
-      
     } catch (error) {
-      console.log(error)
-      
+      console.log(error);
     }
-     
-
-  }
+  };
   return (
     <>
-    <div className="px-10">
+      <div className="md:px-10 relative z-[-1]">
+        <h1 className="text-lg md:text-4xl font-bold py-10 px-4">Contact Us</h1>
+          <div className="grid place-items-center md:grid-cols-3 md:place-items-center py-10">
+            <ContactCard
+              title="Email"
+              img={<AiOutlineMail />}
+              para1="Monday to Friday Expected"
+              para2="response time: 72 hours"
+              value="Send Email"
+            />
+            <ContactCard
+              title="Live Chat"
+              img={<AiOutlineWechat />}
+              para1="Weekdays: 9 AM - 6 PM ET"
+              para2="Weekends: 9 AM - 5 PM ET"
+              value="Chat Now"
+            />
+            <ContactCard
+              title="Email"
+              img={<MdForum />}
+              para1="Monday to Friday Expected"
+              para2="response time: 72 hours"
+              value="Ask The Community"
+            />
+          </div>
 
-      <h1 className="text-4xl font-bold pt-10">Contact Us</h1>
-      <div className="grid grid-cols-3 place-items-center py-24">
-        <ContactCard title="Email" img={<AiOutlineMail/>} para1="Monday to Friday Expected" para2="response time: 72 hours" value="Send Email" />
-        <ContactCard title="Live Chat" img={<AiOutlineWechat/>} para1="Weekdays: 9 AM - 6 PM ET" para2="Weekends: 9 AM - 5 PM ET" value="Chat Now" />
-        <ContactCard title="Email" img={<MdForum/>} para1="Monday to Friday Expected" para2="response time: 72 hours" value="Ask The Community" />
+        <div className="text-center">
+          <h1 className="text-lg md:text-3xl font-bold">
+            We'd love to hear <sapn className="text-red-800">form you</sapn>
+          </h1>
+        </div>
+        <div className="flex justify-center py-4">
+          <ContactForm
+            resStatus={status}
+            handlesubmit={handleSubmit}
+            onchange={handleChange}
+          />
+        </div>
+        <Maps />
       </div>
-      <div className="text-center">
-        <h1 className="text-3xl font-bold">We'd love to hear <sapn className="text-red-800" >form you</sapn></h1>
-      </div>
-      <div className="flex justify-center py-4">
-        <ContactForm resStatus={status} handlesubmit={handleSubmit} onchange={handleChange} />
-      </div>
-      <Maps />
-
-    
-      
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
