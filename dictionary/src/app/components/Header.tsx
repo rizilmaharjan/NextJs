1aae81823585560cde4e0153b931fd21871e8845
rizilmaharjan <rizilmaharjan@gmail.com>
2023-06-02 12:49:40 +0545

    updated header

--- dictionary/src/app/components/Header.tsx
+++ dictionary/src/app/components/Header.tsx
@@ -1,37 +1,36 @@
-"use client"
+"use client";
 import { BiBook } from "react-icons/bi";
 import { MdDarkMode } from "react-icons/md";
 import { CiDark } from "react-icons/ci";
 import { useState } from "react";
-import Input from "./Input"
+import Input from "./Input";
 import classNames from "classnames";
 
 const Header = () => {
-  const [dark, setDark] =  useState(false)
-  const handleDarkMode = ()=>{
-    setDark(!dark)
-  }
+  const [dark, setDark] = useState(false);
+  const handleDarkMode = () => {
+    setDark(!dark);
+  };
   return (
     <>
-    <div className={classNames("h-screen",{
-          "bg-black" : dark ===true,
-          "text-white" : dark ===true,
-        })}>
-
-      <header>
-        <div className="md:w-3/5 md:mx-auto py-4 px-2 flex justify-around items-center">
-          <BiBook className="text-4xl" />
-          {
-              dark ? <MdDarkMode onClick={handleDarkMode} className="text-3xl" /> : <CiDark onClick={handleDarkMode} className="text-3xl" />
-            }
-
-        </div>
-      </header>
-      <Input dark={dark} />
-            </div>
-     
-      
-     
+      <div
+        className={classNames("h-screen", {
+          "bg-black": dark === true,
+          "text-white": dark === true,
+        })}
+      >
+        <header>
+          <div className="md:w-3/5 md:mx-auto py-4 px-2 flex justify-around items-center">
+            <BiBook className="text-4xl" />
+            {dark ? (
+              <MdDarkMode onClick={handleDarkMode} className="text-3xl" />
+            ) : (
+              <CiDark onClick={handleDarkMode} className="text-3xl" />
+            )}
+          </div>
+        </header>
+        <Input dark={dark} />
+      </div>
     </>
   );
 };
